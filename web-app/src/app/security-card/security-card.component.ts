0c1801e0b5e6a108da54029d2d07cb9c39ed8671
Callum Wright <wrightci01w@gmail.com>
2021-02-07 15:36:46 +0000

    Initialise security-card component

---
+++ web-app/src/app/security-card/security-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-security-card',
+  templateUrl: './security-card.component.html',
+  styleUrls: ['./security-card.component.scss'],
+})
+export class SecurityCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
