392510bcb7828a27f8de0fe1bae48139e28104c3
Callum Wright <wrightci01w@gmail.com>
2021-02-09 20:24:33 +0000

    Add security-card summary; sentiment and movement

--- web-app/src/app/components/security-card/security-card.component.ts
+++ web-app/src/app/components/security-card/security-card.component.ts
@@ -10,11 +10,17 @@ export class SecurityCardComponent implements OnInit {
   @Input() description: string = '';
   @Input() imgSrc: string = ''
   @Input() name: string = '';
+  @Input() sentiment: string = '';
   @Input() ticker: string = '';
+  @Input() movement: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  parseFloat(sentiment: string): number {
+    return parseFloat(sentiment);
   }
 
 }
