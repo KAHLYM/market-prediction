dd20e08bf81d9ceb5f8b72281a67ca30ff5e67ad
Callum Wright <wrightci01w@gmail.com>
2021-02-13 17:06:04 +0000

    Add @angular/material. Setup material icons

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SecurityCardComponent } from './components/security-card/security-card.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SecurityCardComponent
+    SecurityCardComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
