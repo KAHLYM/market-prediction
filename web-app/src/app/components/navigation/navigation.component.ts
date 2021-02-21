ea24f8ad6fe01ac0b1ef5be49e43fefb1f4cd889
Callum Wright <wrightci01w@gmail.com>
2021-02-21 15:33:38 +0000

    Make navigation hamburger-style

--- web-app/src/app/components/navigation/navigation.component.ts
+++ web-app/src/app/components/navigation/navigation.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  open: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onMenuClick(): void {
+    this.open = !this.open;
+  }
+
+  onLinkClick(): void {
+    this.open = false;
+  }
 }
