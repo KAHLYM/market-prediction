5cd4bd9820ca637f2f2d7477f9debe0136b61530
Callum Wright <wrightci01w@gmail.com>
2022-01-03 14:31:39 +0000

    #110 Remove DummySearchComponent

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -10,7 +10,6 @@ import {provideAuth, getAuth} from '@angular/fire/auth';
 import {provideFirestore, getFirestore} from '@angular/fire/firestore';
 import {provideStorage, getStorage} from '@angular/fire/storage';
 import {DummyComponent} from './components/dummy/dummy.component';
-import {DummySearchComponent} from './components/dummy-search/dummy-search.component';
 import {SearchService} from './services/search.service';
 import {FooterComponent} from './components/footer/footer.component';
 import {HeaderComponent} from './components/header/header.component';
@@ -22,7 +21,6 @@ import {AngularMaterialModule} from './angular-material.module';
   declarations: [
     AppComponent,
     DummyComponent,
-    DummySearchComponent,
     FooterComponent,
     HeaderComponent,
     SearchComponent,
