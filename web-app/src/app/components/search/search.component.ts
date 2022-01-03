3aa2fd253b6385d7ea5be66a8ac4bc8c24a81773
Callum Wright <wrightci01w@gmail.com>
2022-01-03 13:12:46 +0000

    #110 SignInAnonymously

--- web-app/src/app/components/search/search.component.ts
+++ web-app/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Auth, getAuth, signInAnonymously } from '@angular/fire/auth';
 import {SearchService} from 'src/app/services/search.service';
 
 @Component({
@@ -12,8 +13,21 @@ export class SearchComponent implements OnInit {
 
   results: string[] = [];
 
-  constructor(private searchService: SearchService) {
+  constructor(private searchService: SearchService, auth: Auth) {
     this.searchService = searchService;
+
+    // TODO: Move to be global
+    signInAnonymously(getAuth())
+    .then(() => {
+      console.log("Signed in anonymously");
+      // Signed in..
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Failed to sign in anonymously with error code ", errorCode, "and message ", errorMessage);
+      // ...
+    });
   }
 
   ngOnInit(): void {
@@ -21,6 +35,7 @@ export class SearchComponent implements OnInit {
 
   onKey(event: KeyboardEvent): void {
     this.results = this.searchService.query((<HTMLInputElement>event.target).value);
+    console.log("onKey ", this.results);
   }
 
   onClear(event: MouseEvent): void {
