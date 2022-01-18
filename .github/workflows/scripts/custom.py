8b48ba685c60f02ad43643637bf2c8ced6ee196f
Callum Wright <wrightci01w@gmail.com>
2022-01-18 19:09:23 +0000

    #139 Increase logging

--- .github/workflows/scripts/custom.py
+++ .github/workflows/scripts/custom.py
@@ -1,2 +1,3 @@
 import sys
-print(f"::warning file={sys.argv[1]},line=1,col=1,endColumn=2::Test warning")
+print(f"Running against file: {sys.argv[1]}")
+print(f"::warning file={sys.argv[1]},line=1,col=1,endColumn=2::Test warning against {sys.argv[1]}")
